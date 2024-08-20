3f87036d4f0e90deea17357cdbdc644684d41ce0
NicolasFortunato <nicofortunatocs@gmail.com>
2024-08-20 15:17:08 -0300

    Se agrega el proyecto Papeles Porfavor

---
+++ PapelesPorfavor.py
@@ -0,0 +1,3 @@
+#Papeles Porfavor es la adaptacion criolla del famoso puzzle de simulacion Papers Please.
+#En esta adaptacion nos encontraremos con cambios significativos para el juego tal y como nuevos nombres para
+#los paises, provincias, ciudadanos, nuevos requisitos de acceso y mucho mas!
